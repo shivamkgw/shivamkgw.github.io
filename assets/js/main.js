ec4a416a7237b165f1c38a2e72c42746e39f25ca
Shivam Kumar <68368995+shivamkgw@users.noreply.github.com>
2024-06-03 12:33:56 +0530

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -242,7 +242,20 @@
   const portfolioLightbox = GLightbox({
     selector: '.portfolio-lightbox'
   });
+  
 
+
+
+
+  function switchStyle() {
+    if (document.getElementById('styleSwitch').checked) {
+      document.getElementById('gallery').classList.add("custom");
+      document.getElementById('exampleModal').classList.add("custom");
+    } else {
+      document.getElementById('gallery').classList.remove("custom");
+      document.getElementById('exampleModal').classList.remove("custom");
+    }
+  }
   /**
    * Portfolio details slider
    */
